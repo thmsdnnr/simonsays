453dc435b8ad0011d5f1c0f4d12763b9ba65d6ae
Thomas Danner <thmsdnnr@gmail.com>
2017-02-08 09:49:15 -0600

    cleaned up timeouts and removed clicks added webaudio

--- simon.js
+++ simon.js
@@ -1,173 +1,173 @@
 window.onload=function() {
 
-  let GO= {
+  //init audio context
+  //HTML5 audio vendor prefixing
+  window.AudioContext = window.AudioContext || window.webkitAudioContext;
+
+  let GO = { //Global vars
+  colors: {
+    red: document.querySelector('div#red'),
+    green: document.querySelector('div#green'),
+    blue: document.querySelector('div#blue'),
+    yellow: document.querySelector('div#yellow')
+  },
+  app:document.querySelector('div.app'),
+  colorControls:document.querySelectorAll('div.control'),
+  infoDiv:document.querySelector('div#info'),
+  context:new AudioContext(),
+  gainNode:'',
   round:1, //current round
-  turns:20, //set to 20 for normal simon
+  turns:20, //set to 20 for "normal" simon
   clickCt:0,
+  strictMode:false,
   isOver:false,
-  COLOR_DELAY:500, //in ms
-  sel:["#red","#green","#blue","#yellow"],
-  numberToName:{red:0,green:1,blue:2,yellow:3},
+  sounds:[],
+  sel:["red","green","blue","yellow"],
+  nameToNumber:{red:0,green:1,blue:2,yellow:3},
   randomSimon:Array(0), //initial value
-  strictMode:false};
+  COLOR_DELAY:500, //in ms
+  };
 
-  let meta=document.querySelectorAll('.meta');
-  meta.forEach((i)=>i.addEventListener('click',handleMeta));
-  console.log(meta);
-  computerTurn();
+  GO.gainNode = GO.context.createGain();
+  //load sounds over AJAX and put into buffers
+  let soundNames=['ONE.mp3','TWO.mp3','THREE.mp3','FOUR.mp3'];
+  let soundPromises=soundNames.map((s)=>loadSound(s));
+  //once our sounds are loaded, start the game
+  Promise.all(soundPromises).then(function(s){ GO.sounds=s; gameInit(); });
 
-function handleMeta(e) {
-    console.log('meta',e);
-    console.log(e.currentTarget.id);
-    if(e.currentTarget.id=="strict") {
+  function gameInit() {
+    //add click handlers for controls
+    let meta=document.querySelectorAll('.meta');
+    meta.forEach((i)=>i.addEventListener('click',handleMeta));
+    computerTurn();
+  }
+
+  function computerTurn()
+  {
+    unbindClicks();
+    updateDisplay();
+    GO.randomSimon.push(Math.floor(Math.random()*4));
+    playSequence(()=>bindClicks());
+  }
+
+  function bindClicks() { GO.colorControls.forEach((c)=>c.addEventListener('click', handleClick)); }
+  function unbindClicks() { GO.colorControls.forEach((c)=>c.removeEventListener('click', handleClick)); }
+  function handleMeta(e) {
+    (e.currentTarget.id==="reset") ? reset() : null;
+    if(e.currentTarget.id==="strict") {
       GO.strictMode=!GO.strictMode;
       let sButton=document.querySelector('button#strict');
-      (GO.strictMode) ? sButton.style.backgroundColor='tomato' : sButton.style.backgroundColor='silver';
+      (GO.strictMode) ? sButton.classList.add('enabled') : sButton.classList.remove('enabled');
     }
-    if(e.currentTarget.id=="reset") {reset(); console.log('reset');}
   }
 
-function bindClicks() {
-  let colorControls=document.querySelectorAll('div.control');
-  colorControls.forEach((c)=>c.addEventListener('click', handleClick));
-}
 
-function unbindClicks() {
-  let colorControls=document.querySelectorAll('div.control');
-  colorControls.forEach((c)=>c.removeEventListener('click', handleClick));
-}
-
-function replaySequence() {
-  console.log('replay');
-  playSequence(function(){
-    playerTurn();
-  });
-}
-
-function computerTurn()
-{
-  console.log('computerturn');
-  unbindClicks();
-  updateDisplay();
-  GO.randomSimon.push(Math.floor(Math.random()*4));
-  playSequence(function() {
-    playerTurn();
-  });
-}
-
-function playerTurn() {
-  console.log('playerturn');
-  bindClicks();
-  var interval = setInterval(function(){ //polling to check for next round
-    if ((GO.clickCt==GO.randomSimon.length)&&(!GO.isOver)) {
-    console.log('next round');
-    unbindClicks();
-    clearInterval(interval);
-    nextRound();
-  }},10);
-}
-
-function nextRound() {
-  if (GO.round!==GO.turns)
-  {
-    GO.round++;
-    GO.clickCt=0;
-    console.log("nextRound Round: "+GO.round+" nextRound clickCt: "+GO.clickCt);
-    setTimeout(computerTurn,1000);
+  function nextRound() {
+    if (GO.round!==GO.turns)
+    {
+      GO.round++;
+      GO.clickCt=0;
+      setTimeout(computerTurn,1000);
+    }
+    else
+    {
+      GO.isOver=true;
+      updateDisplay("YOU WON YOU ARE SO GREAT!");
+      setTimeout(reset,1000);
+    }
   }
-  else
-  {
-    GO.isOver=true;
-    document.querySelector('div#info').innerHTML="YOU WON YOU ARE SO GREAT!";
-    setTimeout(reset,1000);
-  }
-}
 
-function handleClick(e) { //logic to play sounds and check click inputs
-  console.log('clicked on',e.target.id);
-  var selector='#'+e.target.id;
-  playTile(selector,null);
+  function handleClick(e) { //logic to play sounds and check click inputs
+    let expected = GO.randomSimon[GO.clickCt]; // correct next tile
+    let actual = GO.nameToNumber[e.target.id]; // user-clicked tile
+    //let selector=e.target.id;
+    playTile(e.target.id);
     if (GO.clickCt<GO.randomSimon.length)
     {
-      var expected = GO.randomSimon[GO.clickCt];
-      var actual = GO.numberToName[e.target.id]; // input value
-
-      if (actual!=undefined) // will be undefined if click on board but not on a tile with a target
-      {
-        if (expected===actual)
-        {
-          console.log("right");
-          console.log("clickCt: "+GO.clickCt+" expected: "+expected+" actual: "+actual);
-          GO.clickCt++;
-        }
-        else
-        {
-          console.log("wrong");
-          console.log("clickCt: "+GO.clickCt+" expected: "+expected+" actual: "+actual);
-          //repeat playing the sequence if not in strict mode, otherwise reset and start again
-          if (GO.strictMode)
-          {
-            unbindClicks();
-            document.querySelector('div#info').innerHTML="YOU LOST :( NOW WE START AGAIN.";
-            setTimeout(reset,2000);
-          }
-          else
-          {
-            GO.clickCt=0;
-            unbindClicks();
-            document.querySelector('div#info').innerHTML="Eh, not quite. Try again!";
-            setTimeout(function(){replaySequence(); updateDisplay();},1000);
-          }
-        }
+      if (expected===actual) { GO.clickCt++; } //correct tile clicked, keep on a'going
+    else { //wrong tile clicked
+      if (GO.strictMode) { //if strict mode, reset and start again
+        unbindClicks();
+        updateDisplay("YOU LOST :( NOW WE START AGAIN.");
+        setTimeout(reset,2000);
       }
-      else {
-        console.log("UNDEFINED");
-        console.log("clickCt: "+GO.clickCt+" expected: "+expected+" actual: "+actual);
+      else { //repeat the sequence if not in strict mode and let the user try again
+        GO.clickCt=0; //restart the user's input sequence
+        unbindClicks();
+        updateDisplay("Eh, not quite. Try again!");
+        setTimeout(replaySequence,1000);
       }
     }
   }
-
-function reset() {
-  GO.round=1;
-  GO.clickCt=0;
-  GO.isOver=false;
-  GO.randomSimon=Array(0);
-  computerTurn();
+  if ((GO.clickCt==GO.randomSimon.length)&&(!GO.isOver)) { //time for the next round
+    unbindClicks();
+    nextRound();
+  }
 }
 
-function updateDisplay() {
-  document.querySelector('div#info').innerHTML="Current Round: "+GO.round;
-}
+  function reset() {
+    GO.round=1;
+    GO.clickCt=0;
+    GO.isOver=false;
+    GO.randomSimon=Array(0);
+    computerTurn();
+  }
 
-function playSequence(cb) {
-  for (var i=0;i<GO.randomSimon.length;i++)
-  {
-    let selector=GO.sel[GO.randomSimon[i]];
+  function updateDisplay(message) {
+    let m=message||`Current Round: ${GO.round}`;
+    GO.infoDiv.innerHTML=m;
+  }
+
+  //Functions for playing and replaying Simon Sequence
+  function replaySequence() {
+      playSequence(()=>{
+      bindClicks();
+      updateDisplay();
+    });
+  }
+
+  function playSequence(cb) {
+    GO.app.classList.remove('enabled');
+    for (var i=0;i<GO.randomSimon.length;i++)
+    {
+      let selector=GO.sel[GO.randomSimon[i]];
+      setTimeout(()=>playTile(selector),i*GO.COLOR_DELAY+10);
+    }
     setTimeout(function(){
-      playTile(selector);
-    },i*GO.COLOR_DELAY+100);
+      cb();
+      GO.app.classList.add('enabled');
+    },GO.randomSimon.length*(GO.COLOR_DELAY+20));
   }
-  setTimeout(function(){
-    cb();
-  },GO.randomSimon.length*GO.COLOR_DELAY);
-}
 
-function playTile(sel, cb) {
-  document.querySelector(sel).classList.remove('animated');
-  setTimeout(function(){
-    var color=sel.slice(1);
-    document.querySelector(sel).classList.add('animated');
-    var red = document.getElementById("s_red");
-    var green = document.getElementById("s_green");
-    var blue = document.getElementById("s_blue");
-    var yellow = document.getElementById("s_yellow");
-    var colors = {red:red,green:green,blue:blue,yellow:yellow};
-    colors[color].currentTime=0;
-    colors[color].play();
-    colors[color].addEventListener('ended',function(){
-      document.querySelector(sel).classList.remove('animated');
-      return (cb && typeof cb == "function") ? cb() : null;
+  //Play one tile with sound, accepts optional callback to execute on completion
+  function playTile(color, cb) {
+    let thisColor=GO.colors[color];
+    thisColor.classList.add('animated');
+    setTimeout(function(){thisColor.classList.remove('animated');},GO.COLOR_DELAY-100);
+      //buffer the sound, play the sound
+      let b = GO.context.createBufferSource();
+      b.buffer = GO.sounds[GO.nameToNumber[color]];
+      b.connect(GO.context.destination);
+      b.start(0);
+      //by ramping the amplitude we can eliminate some "click" noises on the samples
+      GO.gainNode.gain.setTargetAtTime(1, GO.context.currentTime, 0.05);
+      GO.gainNode.gain.setTargetAtTime(0.01, GO.context.currentTime, + b.buffer.duration-0.01);
+      b.onended = function() {
+        return (cb && typeof cb == "function") ? cb() : null;
+      }
+    }
+
+  function loadSound(soundPath) {
+    return new Promise(function(resolve, reject) {
+      let getSound = new XMLHttpRequest();
+        getSound.open("GET", `sounds/${soundPath}`, true);
+        getSound.responseType = "arraybuffer";
+        getSound.onload = function() {
+          GO.context.decodeAudioData(getSound.response, function(buffer){
+            resolve(buffer);
+          });
+        }
+        getSound.send();
       });
-    },100);
-  }
+    }
 }
